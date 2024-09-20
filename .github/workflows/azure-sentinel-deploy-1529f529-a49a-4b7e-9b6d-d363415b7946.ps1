0e5c64317d956e21feb2e46ef6b29941c0b775fe
Sentinel <donotreply@microsoft.com>
2024-09-20 15:47:09 -0700

    reverting and simplifying

--- .github/workflows/azure-sentinel-deploy-1529f529-a49a-4b7e-9b6d-d363415b7946.ps1
+++ .github/workflows/azure-sentinel-deploy-1529f529-a49a-4b7e-9b6d-d363415b7946.ps1
@@ -13,7 +13,6 @@ $contentTypeMapping = @{
     "Parser"=@("Microsoft.OperationalInsights/workspaces/savedSearches");
     "Playbook"=@("Microsoft.Web/connections", "Microsoft.Logic/workflows", "Microsoft.Web/customApis");
     "Workbook"=@("Microsoft.Insights/workbooks");
-    "BicepResource"=@("Microsoft.Graph/applications@v1.0"); # This would eventually be something such as a Custom detection rule
 }
 $sourceControlId = $Env:sourceControlId 
 $rootDirectory = $Env:rootDirectory
@@ -39,7 +38,6 @@ $sentinelResourcePatterns = @{
     "Parser" = "/subscriptions/$guidPattern/resourceGroups/$namePattern/providers/Microsoft.OperationalInsights/workspaces/$namePattern/savedSearches/$namePattern"
     "Playbook" = "/subscriptions/$guidPattern/resourceGroups/$namePattern/providers/Microsoft.Logic/workflows/$namePattern"
     "Workbook" = "/subscriptions/$guidPattern/resourceGroups/$namePattern/providers/Microsoft.Insights/workbooks/$namePattern"
-    # "BicepTest" = "/subscriptions/$guidPattern/resourceGroups/$namePattern/providers/Microsoft.OperationalInsights/workspaces/$namePattern/providers/Microsoft.SecurityInsights/alertRules/$namePattern"
 }
 
 if ([string]::IsNullOrEmpty($contentTypes)) {
@@ -315,15 +313,7 @@ function IsRetryable($deploymentName) {
     }
 }
 
-function IsValidResourceType($template, $isBicepResource = $false) {
-    if ($isBicepResource) {
-        return IsValidBicepResourceType $template
-    } else {
-        return IsValidArmResourceType $template
-    }
-}
-
-function IsValidArmResourceType($template) {
+function IsValidResourceType($template) {
     try {
         $isAllowedResources = $true
         $template.resources | ForEach-Object { 
@@ -335,10 +325,6 @@ function IsValidArmResourceType($template) {
         $isAllowedResources = $false
     }
     return $isAllowedResources
-}
-
-function IsValidBicepResourceType($template) {
-    return $true # Implement later
 }
 
 function DoesContainWorkspaceParam($templateObject) {
@@ -506,16 +492,14 @@ function Deployment($fullDeploymentFlag, $remoteShaTable, $tree) {
                 Write-Host "[Warning] Skipping deployment for $path. The file doesn't exist."
                 return
             }
-
-            $isBicep = $false
+            
             if ($path -like "*.bicep") {
                 $templateObject = bicep build $path --stdout | Out-String | ConvertFrom-Json
-                $isBicep = $true
             } else {
                 $templateObject = Get-Content $path | Out-String | ConvertFrom-Json
             }
 
-            if (-not (IsValidResourceType $templateObject $isBicep))
+            if (-not (IsValidResourceType $templateObject))
             {
                 Write-Host "[Warning] Skipping deployment for $path. The file contains resources for content that was not selected for deployment. Please add content type to connection if you want this file to be deployed."
                 return
